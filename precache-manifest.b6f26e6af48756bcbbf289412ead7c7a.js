2edd91322fd20eb8c7b53eefea6370ff5b9e9d14
BIP <satyam0150@gmail.com>
2022-06-15 00:25:06 +0530

    Updates

---
+++ precache-manifest.b6f26e6af48756bcbbf289412ead7c7a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a45ad9bd5064029db61",
+    "url": "/U-deline/static/css/main.5aac3060.chunk.css"
+  },
+  {
+    "revision": "8a45ad9bd5064029db61",
+    "url": "/U-deline/static/js/main.64a86f07.chunk.js"
+  },
+  {
+    "revision": "7f5dc4a736913415f2f4",
+    "url": "/U-deline/static/js/runtime~main.a306f3b6.js"
+  },
+  {
+    "revision": "45da55deb80be8fe1db8",
+    "url": "/U-deline/static/css/2.81a8bbb6.chunk.css"
+  },
+  {
+    "revision": "45da55deb80be8fe1db8",
+    "url": "/U-deline/static/js/2.0aba73c2.chunk.js"
+  },
+  {
+    "revision": "b390a36d279b692bc1d3",
+    "url": "/U-deline/static/js/3.e61989fa.chunk.js"
+  },
+  {
+    "revision": "aa4e97e3ceadf6a6d309610912d5383b",
+    "url": "/U-deline/static/media/mentorbg.aa4e97e3.png"
+  },
+  {
+    "revision": "8f98df26d1c997b762f6da071a99707b",
+    "url": "/U-deline/static/media/footerbg1.8f98df26.png"
+  },
+  {
+    "revision": "1933c5b9cb7940051ce71d096b7f8ffe",
+    "url": "/U-deline/index.html"
+  }
+];
